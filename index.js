dc8ac6e49a76496db637a2bcba6b708df6950ea8
Ingrid Wong <yw340@georgetown.edu>
2017-12-13 20:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Ingrid"
-var height = "156"
+var name = "Ingrid";
+var height = "156";
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(parseInt(height)+1);
